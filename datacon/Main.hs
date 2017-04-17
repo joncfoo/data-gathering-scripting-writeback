b80e794a7b7ea95bcd22336864f5d270d874b514
Jonathan Curran <jonathan@curran.in>
2017-04-16 22:54:30 -0500

    Dirty: use separate consumer thread

--- datacon/Main.hs
+++ datacon/Main.hs
@@ -1,15 +1,28 @@
 import Protolude
+import Control.Concurrent (threadDelay)
+import Data.Map.Strict (Map)
 import System.IO (hClose)
 
+import qualified Data.Map.Strict as Map
 import qualified Network.Fancy as N
 import qualified System.IO.Streams as S
 
 
+consumer :: MVar (Map Text Text) -> IO ()
+consumer cells = forever $
+  do
+    content <- readMVar cells
+    putText (content & Map.showTree & toS)
+    threadDelay (1000 * 1000)
+
+
 main :: IO ()
 main = do
+  cells <- newMVar Map.empty
   h <- N.connectStream (N.IP "localhost" 5000)
   (is, os) <- S.handleToStreams h
-  tId <- forkIO (handleWrite is)
+  tId <- forkIO (handleWrite is cells)
+  forkIO $ consumer cells
   finally (handleRead os tId) (hClose h)
 
 
@@ -30,8 +43,13 @@ toInt :: ByteString -> Maybe Int
 toInt = readMaybe . toS
 
 
-handleWrite :: S.InputStream ByteString -> IO ()
-handleWrite is =
+handleWrite :: S.InputStream ByteString -> MVar (Map Text Text) -> IO ()
+handleWrite is cells =
   S.mapMaybe toInt is
-  >>= S.map ((<> "\n") . show)
+  >>= S.map show
+  >>= S.mapM fn
   >>= S.connectTo S.stdout
+  where
+    fn v =
+      swapMVar cells (Map.singleton "5000" (toS v))
+      >> pure v
